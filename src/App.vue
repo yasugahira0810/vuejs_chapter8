736d488f905a3cbeff248d9d3d8be6a176ef7c54
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2018-09-07 16:42:23 +0900

    Implement getter

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div class="app">
+    <h3>引数なし</h3>
+    <ol>
+      <li>{{ count }}</li>
+      <li>{{ max }}</li>
+    </ol>
+    <h3>引数付き</h3>
+    <ol>
+      <li>{{ itemA }}</li>
+      <li>{{ itemB(1) }}</li>
+      <li>{{ nameA }}</li>
+      <li>{{ nameB(1) }}</li>
+    </ol>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import store from './store.js'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    // 引数なしゲッター
+    count() { return store.getters.count },   // 1
+    max()   { return store.getters.max },     // 2
+    // 引数付きゲッター
+    itemA() { return store.getters.item(1) }, // 1 👍 いいね
+    itemB() { return store.getters.item },    // 2 👎 よくないね
+    nameA() { return store.getters.name(1) }, // 3 👍 いいね
+    nameB() { return store.getters.name },    // 4 👎 よくないね
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
