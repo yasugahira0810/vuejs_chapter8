0b769237ae1ca685074b915875cfc21f2e31bbf2
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2018-09-07 14:42:00 +0900

    Implement Simple Store.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import store from '@/store.js'
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+console.log(store.state.count) // -> 0
+// incrementをコミットする
+store.commit('increment')
+// もう一度アクセスしてみるとカウントが増えている
+console.log(store.state.count) // -> 1
--- src/store.js
+++ src/store.js
@@ -3,3 +3,17 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 // プラグインとして登録
 Vue.use(Vuex)
+
+// ストアを作成
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    // カウントアップするミューテーションを登録
+    increment(state) {
+      state.count++
+    }
+  }
+})
+export default store
