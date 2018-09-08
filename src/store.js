a3b6bb75967c55bf490e6f9262b9a1cbd5492f7a
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2018-09-09 00:23:28 +0900

    Reuse modules

--- src/store.js
+++ src/store.js
@@ -2,8 +2,32 @@ import moduleA from '@/store/a.js'
 import 'babel-polyfill'
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 // プラグインとして登録
 Vue.use(Vuex)
+Vue.use(VueAxios, axios)
+
+const myModule = {
+  namespaced: true,
+  state() {
+    return {
+      entries: []
+    }
+  },
+  mutations: {
+    set(state, payload) {
+      state.entries = payload
+    }
+  },
+  actions: {
+    load({ commit }, file) {
+      axios.get(file).then(response => {
+        commit('set', response.data)
+      })
+    }
+  }
+}
 
 const moduleB = {
   namespaced: true,
@@ -15,7 +39,9 @@ const moduleB = {
 const store = new Vuex.Store({
   modules: {
     moduleA,
-    moduleB
+    moduleB,
+    moduleC: myModule,
+    moduleD: myModule
   },
   getters: {
     user() { return 'getter: user' }
@@ -31,5 +57,9 @@ const store = new Vuex.Store({
 // 何が呼び出されるか、コンソールログを確認してみよう
 store.dispatch('moduleA/test')
 console.log(store.getters['moduleA/test'])
+
+// 別のデータを読み込んだりする
+store.dispatch('moduleC/load', '/static/a.json')
+store.dispatch('moduleD/load', '/static/b.json')
 
 export default store
